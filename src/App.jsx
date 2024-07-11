1f6fe188d3cfd676ad64b40fd065a5a0abb839b4
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-07-11 11:22:13 -0300

    Adicionei os contadores com UseState

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import { NewTask } from "./components/NewTask.jsx"
 
 function App() { 
 
+    const [TaskCreate, setTaskCreate] = useState(0);
+    const [TaskComplete, setTaskComplete] = useState(0);
+
+
   return (
     <>
       <div>
@@ -21,23 +25,25 @@ function App() {
          id="new-task"
          placeholder="Adicionar uma nova tarefa"/>
 
-          <button className={styles.button}>Criar</button>
+          <button className={styles.button} onClick={() => setTaskCreate(TaskCreate + 1)}>Criar</button>
          </div>
 
         <div className={styles.info}>
-          <h2 className={styles.TaskedInfoCreate}>Tarefas criadas <p className={styles.contador}>1</p> </h2>
-          <h2 className={styles.TaskedInfoCompleted}>Tarefas Concluídas <p className={styles.contador}>1</p> </h2>
+          <h2 className={styles.TaskedInfoCreate}>Tarefas criadas <p className={styles.contador}>{TaskCreate}</p> </h2>
+          <h2 className={styles.TaskedInfoCompleted}>Tarefas Concluídas <p className={styles.contador}>{TaskComplete}</p> </h2>
         </div>
           <hr />
           <div>
           <NewTask
           content="Passear com os cachorros"
+          check={() => setTaskComplete(TaskComplete + 1)}
           />
           </div>
       </div>
     </>
-  )
-  }
+    )
+}
+
 
 export default App
 
--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -3,7 +3,7 @@ import styles from './NewTask.module.css'
 export function NewTask(props) {
     return (
         <div className={styles.NewTask}>
-        <input type="checkbox" name="" id="" />  <p>{props.content}</p>
+        <input type="checkbox" name="" id="" onClick={props.check} />  <p>{props.content}</p>
         </div>
     )
 }
